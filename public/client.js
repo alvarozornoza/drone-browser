59ff39e169c3d55a6fc9289a004f81926b00709c
alvarozornoza <azuzornoz@gmail.com>
2018-11-24 16:28:08 +0100

    Reloj

--- public/client.js
+++ public/client.js
@@ -134,6 +134,23 @@
   $("*[rel=tooltip]").tooltip();
 
   window.onload = function() {
+    
+    function startTime() {
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+        m = checkTime(m);
+        s = checkTime(s);
+        document.getElementById('txt').innerHTML = 
+        h + ":" + m + ":" + s;
+        var t = setTimeout(startTime, 500);
+    }
+    startTime();
+    function checkTime(i) {
+        if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+        return i;
+    }
     if (annyang) {
       // Let's define a command.
       var commands = {
